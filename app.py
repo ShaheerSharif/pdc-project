a096a0ac258956cfe4defc3c3344f30a75add623
Shaheer Sharif <sharfshaheer@gmail.com>
2025-01-16 02:15:53 +0500

    Added orders and users

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+from os import read
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 import json
 
@@ -7,80 +8,122 @@ app = Flask(__name__)
 # Sample data structure to hold inventory items
 inventory = []
 
-JSON_FILE_PATH="inventory.json"
-
-
-def get_table_info():
-    return (request.form["name"], int(request.form["quantity"]), round(float(request.form["price"]), 2))
+INVENTORY_FILE_PATH = "db/inventory.json"
+ORDERS_FILE_PATH = "db/orders.json"
+USERS_FILE_PATH = "db/users.json"
 
 
 def read_inventory():
     try:
-        with open(JSON_FILE_PATH, 'r') as file:
+        with open(INVENTORY_FILE_PATH, "r") as file:
             return json.load(file)
     except (FileNotFoundError, json.JSONDecodeError):
         return []
 
 
 def save_inventory(inventory):
-    with open(JSON_FILE_PATH, 'w') as file:
+    with open(INVENTORY_FILE_PATH, "w") as file:
         json.dump(inventory, file, indent=4)
 
 
+def read_users():
+    try:
+        with open(USERS_FILE_PATH, "r") as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
 
-@app.route('/')
-def index():
-    inventory = read_inventory()
-    return render_template('index.html', inventory=inventory)
+
+def save_users(users):
+    with open(USERS_FILE_PATH, "w") as file:
+        json.dump(users, file, indent=4)
 
 
-@app.route('/add', methods=['GET', 'POST'])
+def get_inventory_info():
+    return (
+        request.form["name"],
+        int(request.form["quantity"]),
+        round(float(request.form["price"]), 2),
+    )
+
+
+def get_order_info():
+    return (
+        request.form["name"],
+        [tuple(request.form["products"])],
+        request.form["date"],
+    )
+
+
+@app.route("/", method=["GET", "POST"])
+def signin():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        users = read_users()
+        user = next(
+            (
+                user
+                for user in users
+                if user["username"] == username and user["password"] == password
+            ),
+            None,
+        )
+
+    if user:
+        flash("Sign-in successful!", "success")
+        return redirect(url_for("signin"))  # Redirect to a dashboard or homepage
+    else:
+        flash("Invalid username or password!", "error")
+        return redirect(url_for("signin"))
+
+    return render_template("signin.html")
+
+
+@app.route("/add-inv", methods=["GET", "POST"])
 def add_item():
-    if request.method == 'POST':
-        # item_name = request.form['name']
-        # item_quantity = request.form['quantity']
-        # item_price = request.form['price']
-
-        (name, quantity, price) = get_table_info()
+    if request.method == "POST":
+        (name, quantity, price) = get_inventory_info()
 
         inventory = read_inventory()
-        inventory.append({'name': name, 'quantity': quantity, 'price': price})
+        inventory.append({"name": name, "quantity": quantity, "price": price})
         save_inventory(inventory)
-        
-        return redirect(url_for('index'))
-    return render_template('add-item.html')
+
+        return redirect(url_for("admin/index"))
+    return render_template("admin/add-item.html")
 
 
-@app.route('/update/<int:item_id>', methods=['GET', 'POST'])
+@app.route("/update-inv/<int:item_id>", methods=["GET", "POST"])
 def update_item(item_id):
     inventory = read_inventory()
 
-    if request.method == 'POST':
-        # name = request.form['name']
-        # quantity = int(request.form['quantity'])
-        # price = float(request.form['price'])
+    if request.method == "POST":
+        (name, quantity, price) = get_inventory_info()
 
-        (name, quantity, price) = get_table_info()
-
-        inventory[item_id] = {'name': name, 'quantity': quantity, 'price': price}
+        inventory[item_id] = {"name": name, "quantity": quantity, "price": price}
 
         save_inventory(inventory)
 
-        return redirect(url_for('index'))
+        return redirect(url_for("admin/index"))
 
     item = inventory[item_id]
-    return render_template('update-item.html', item=item, item_id=item_id)
+    return render_template("admin/update-item.html", item=item, item_id=item_id)
 
 
-
-@app.route('/delete/<int:item_id>')
+@app.route("/delete-inv/<int:item_id>")
 def delete_item(item_id):
     inventory = read_inventory()
     inventory.pop(item_id)
     save_inventory(inventory)
 
-    return redirect(url_for('index'))
+    return redirect(url_for("admin/index"))
 
 
-if __name__ == '__main__':
+@app.route("/logout")
+def logout():
+    return redirect(url_for("signin"))
+
+
+if __name__ == "__main__":
     app.run(debug=True)
